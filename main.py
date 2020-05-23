abfae3f1613b353e87d863d09da0a454c58d8a72
gouand96 <andrei.grosu.96@mail.ru>
2020-05-23 22:59:17 +0300

    App translated to English

--- main.py
+++ main.py
@@ -19,45 +19,40 @@ import os
 
 load_dotenv()
 
-
 token = os.getenv('TOKEN')
 appid = os.getenv('APPID')
 bot = telebot.TeleBot(token)
 
-
-settings = {"APPID": appid,"units": "metric", "lang": "RU"}
-
-
-
+settings = {"APPID": appid, "units": "metric", "lang": "EN"}
 
 
 @bot.message_handler(commands=['start'], content_types=["text"])
 def echo_all(message):
     bot.send_chat_action(message.chat.id, "typing")
-    #bot.send_message(message, message.text)
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=3)
-    itembtn1 = types.KeyboardButton(text='Погода')
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.KeyboardButton(text='Whether')
     itembtn2 = types.KeyboardButton(text='Youtube')
     markup.add(itembtn1, itembtn2)
-    bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
+    bot.send_message(message.chat.id, "Chose command:", reply_markup=markup)
+
 
 @bot.message_handler(content_types=["text"])
 def check_btn(message):
     getMessage = message.text.strip().lower()
 
-    if getMessage == "погода":
-        data = owm.get_current("Кишинев", **settings)
+    if getMessage == "whether":
+        data = owm.get_current("Chisinau", **settings)
         temp = data("main.temp")
         feels = data("main.feels_like")
         name = data("name")
         whether = data["weather"][0]["description"]
 
         print(data)
-        bot.send_message(message.chat.id, "Температура воздуха в Кишиневе сейчас " + str(temp)
-                         + " градусов\nЧувствуется как " + str(feels) + " градусов\n"+
-                          "На улице " + whether)
+        bot.send_message(message.chat.id, "Air temperature in Chisinau is " + str(temp)
+                         + " degree\nFeeling as " + str(feels) + " degree\n" +
+                         "On outdoors is  " + whether)
     elif getMessage == "youtube":
-        bot.send_message(message.chat.id, "Введите ссылку на видео и нажмите Enter: ")
+        bot.send_message(message.chat.id, "Enter a Youtube video link and tap Enter key: ")
     elif str(message.text.strip()).find("youtube.com") != -1:
         try:
 
@@ -66,8 +61,8 @@ def check_btn(message):
             br.open(url)
             title = br.title()
             print(title)
-        except (mechanize.URLError,mechanize.HTTPError) as e:
-            bot.send_message(message.chat.id, "Ошибка, нет такого ресурса")
+        except (mechanize.URLError, mechanize.HTTPError) as e:
+            bot.send_message(message.chat.id, "Error! Can`t get this resource!")
 
         try:
             ydl_opts = {
@@ -79,21 +74,20 @@ def check_btn(message):
                 }],
             }
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                bot.send_message(message.chat.id, "Подождите немного... Видео обрабатывается")
+                bot.send_message(message.chat.id, "Wait a bit... Video is being processed")
                 print(url)
-                destionation = ydl.download([url])
+                ydl.download([url])
                 parser = urlparse.urlparse(url)
                 param = parse_qs(parser.query)['v']
                 name = str(title).replace(" - YouTube", "-" + str(param).replace("['", "").replace("']", ""))
                 audio = open(name + ".mp3", 'rb')
                 bot.send_audio(message.chat.id, audio)
-        except (youtube_dl.DownloadError,telebot.apihelper.ApiException):
-            bot.send_message(message.chat.id, "Ошибка скачивания, неправильная ссылка либо слижком большой файл!")
-        # else:
-        #     bot.send_message(message.chat.id, "Неправильная ссылка")
-    elif message.text.strip().lower() == "спасибо":
-        bot.send_message(message.chat.id, "Это тебе спасибо:)")
+        except (youtube_dl.DownloadError, telebot.apihelper.ApiException):
+            bot.send_message(message.chat.id, "Error! This link is incorrect or this video is very large!")
+    elif message.text.strip().lower() == "Thanks!":
+        bot.send_message(message.chat.id, "Thanks you too:)")
     else:
-        bot.send_message(message.chat.id, "Неправильная команда")
+        bot.send_message(message.chat.id, "Command is incorrect!")
+
 
 bot.polling()
